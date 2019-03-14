88ba2d7bc231a87934b67c54859ae7019ec8102a
Liupengcheng <liupengcheng@xiaomi.com>
2019-03-14 11:36:26 +0800

    Copy tpcds data gen scripts and class here to make datagen and benchmark together

--- examples/tpcds/src/main/scala/org/apache/spark/sql/benchmark/Utils.scala
+++ examples/tpcds/src/main/scala/org/apache/spark/sql/benchmark/Utils.scala
@@ -17,7 +17,8 @@
 
 package org.apache.spark.sql.benchmark
 
-import java.io.{ByteArrayOutputStream, File, FileInputStream, InputStream, PrintWriter}
+import java.io._
+import java.util.UUID
 
 import scala.io.Source
 
@@ -260,5 +261,42 @@ private[benchmark] object Utils {
     }
 
     sb.toString
+  }
+
+  /**
+   * Create a directory inside the given parent directory. The directory is guaranteed to be
+   * newly created, and is not marked for automatic deletion.
+   */
+  private def createDirectory(root: String, namePrefix: String = "spark"): File = {
+    var attempts = 0
+    val maxAttempts = 10
+    var dir: File = null
+    while (dir == null) {
+      attempts += 1
+      if (attempts > maxAttempts) {
+        throw new IOException("Failed to create a temp directory (under " + root + ") after " +
+          maxAttempts + " attempts!")
+      }
+      try {
+        dir = new File(root, namePrefix + "-" + UUID.randomUUID.toString)
+        if (dir.exists() || !dir.mkdirs()) {
+          dir = null
+        }
+      } catch { case e: SecurityException => dir = null; }
+    }
+
+    dir.getCanonicalFile
+  }
+
+  /**
+   * Create a temporary directory inside the given parent directory. The directory will be
+   * automatically deleted when the VM shuts down.
+   */
+  def createTempDir(
+      root: String = System.getProperty("java.io.tmpdir"),
+      namePrefix: String = "spark"): File = {
+    val dir = createDirectory(root, namePrefix)
+    dir.deleteOnExit()
+    dir
   }
 }
